b4698ee880c4abd1465c2e56006a526fa36498ed
Chang <xiangxiang3451@gmail.com>
2023-10-14 15:51:48 +0700

    lab5-finished

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-function myClick(){
-    alert(9);
+function firstClick() {
+    var tag = document.getElementById('box');
+    tag.style.display = 'flex';
+
+}
+function secondClick(){
+    var tag = document.getElementById('box');
+    document.getElementById('firstNum').innerHTML='2';
+    document.getElementById('secondNum').innerHTML='2';
+    tag.style.display = 'flex';
+    
+}
+function thirdClick(){
+    var tag = document.getElementById('box');
+    document.getElementById('firstNum').innerHTML='3';
+    document.getElementById('secondNum').innerHTML='3';
+    tag.style.display = 'flex';
+}
+function show(){
+    var tag = document.getElementById('box');
+    tag.style.display = 'none';
 }
